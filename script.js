ccf4f15cb14b9717beb30514ba1e4defa407a7f7
dradge12 <aleksey.xr2350@gmail.com>
2024-02-17 23:05:18 +0100

    start js basics

---
+++ script.js
@@ -0,0 +1,23 @@
+// let age = 16
+// let parentConsent = true
+
+// if(age >= 18 || parentConsent) {
+//     console.log("Доступ разрешен")
+// } else {
+//     console.log("Доступ запрещён")
+// }
+
+let sunny = "sunny"
+let cloudy = 'cloudy'
+let rainy = "rainy"
+
+let weatherForecast = cloudy
+
+let temperature = 25
+
+let freeTime = true
+
+if ((weatherForecast === cloudy || weatherForecast === sunny) && (temperature >= 20 && temperature <= 30) && freeTime) {
+    console.log("Идеальный день для пикника!")
+}
+
